7ca1f067f8e05bc850cb9883b05bd624b2f30a83
noel <noelboban9@>
2021-02-22 17:52:46 -0500

    Initial Commit

---
+++ Learingex/LearingexApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingexApp.swift
+//  Learingex
+//
+//  Created by Noel Boban on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct LearingexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
